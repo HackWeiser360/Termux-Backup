7cf4c302bdde50455c86cb7cb9eab4da1eb15f17
MådMâx <74961526+HackWeiser360@users.noreply.github.com>
2020-12-27 15:35:09 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # Termux-Backup
-A tool for backing up and restoring your Termux data💯
+Termux-Backup is a bash based tool for backing up and restoring Termux data. It works in both rooted and non rooted devices.Perfect!!! I tested the tool in Termux only but you can try installing it in Kali or another operating system.
+
+***
+## [~] Installation and Usage
+
+* $ apt update 
+* $ apt upgrade 
+* $ pkg install python 
+* $ pkg install python2 
+* $ pkg install git 
+* $ pip install lolcat
+* $ git clone https://github.com/HackWeiser360/Termux-Backup
+* $ cd $HOME
+* $ ls
+* $ cd Termux-Backup
+* $ ls
+* $ bash Termux-Backup.sh
+***
+[+] You can select any option by clicking on your keyboard
+## USAGE OPTIONS [Termux] :
+
+__TERMUX BACKUP__ :
+- From this option you can backup all the tools and default bash terminal script into your internal stoarge without any issue and you can restore it at any time.
+
+__TERMUX RESTORE__ :
+- From this option you can restore the backup tools of termux and default terminal as it was first.
+
+__UPDATE SCRIPT__ :
+- From this option you can update Termux-Backup tool if updates are available for that.
+
+__EXIT__ :
+- From this option you can exit from Termux-Backup tool.
+### [+] Follow on [Instagram](https://www.instagram.com/madmax4708/)
+### [+] Visit our YouTube channel. [Why not?](https://youtube.com/channel/UC02OkpTZkxRZCEzFjawf6mA)
+### [+] Visit our [FaceBook Page](fb.me/HackWeiser360)
+### [+] You can leave a message [here](m.me/HackWeiser360)
---
+++ Termux-Backup.sh
@@ -0,0 +1,154 @@
+# A tool by nitrp from www.HackWeiser360.com
+# Be a unique person not copy cat
+# Colours
+#-----------------
+red='\033[1;31m'
+rset='\033[0m'
+grn='\033[1;32m'
+ylo='\033[1;33m'
+blue='\033[1;34m'
+cyan='\033[1;36m'
+pink='\033[1;35m'
+#-----------------
+# Coding starts
+clear
+echo '
+                        .--.    .--,
+                        |  |   /  /
+                        |  |  /  /
+                        |  | /  /
+                        |  |/  ;-._
+                        |  ` _/  / ;
+                        |  /` ) /  /
+                        | /  /_/\_/\
+                        |/  /      |
+                        (    \  -  |
+                         \    `.  /
+                          |      |
+                          |      |
+                                   _
+                          _ __  _ |_)
+                         _> | |(_||v1.0 '|lolcat
+echo 
+
+echo 
+# Functions
+echo -e "$red              Created by HackWeiser360[madmax]$rset"
+echo " "
+echo " "
+echo -e "$grn                          > $ylo [Options]$grn <$rset"
+echo " "
+echo " "
+echo -e "$red                        ➡$cyan [1] Termux backup"
+echo -e "$red                        ➡$cyan [2] Termux restore"
+echo -e "$red                        ➡$cyan [3] Update script"
+echo -e "$red                        ➡$cyan [4]️ Exit"
+echo " "
+echo " "
+echo -e "$grn                         >$ylo [SELECT]$grn <$rset"
+echo " "
+echo -e "$cyan                       [$ylo Select any option$cyan]$rset"
+echo " "
+read n
+case "$n" in
+1)echo " "
+echo " "
+echo " "
+echo -e "$red               You are going to$grn Backup$red Termux$rset"
+echo " "
+echo -e "$grn                  Press$ylo ENTER$red to continue$rset"
+echo " "
+read hulke
+cd $HOME/Termux-Backup/bars
+echo " "
+echo -e "$ylo     To terminate the process click$red 't'$ylo or to continue click on$grn ENTER$rset" 
+read choice
+if [ $choice = 't' ] ; then
+echo -e "$red                    Are you sure? Press$grn ENTER$red to exit$rset"
+read hulkee
+cd $HOME/Termux-Backup/
+bash Termux-Backup.sh
+else
+bash Termux-Backup.sh
+echo " "
+echo " "
+echo -e "$grn ＞＞＞＞＞＞＞＞＞$ylo [EXIT]$grn ＜＜＜＜＜＜＜＜＜$rset"
+echo " "
+echo -e "$cyan            click$grn ENTER$cyan to go back into Termux-Backup menu$rset"
+echo " "
+read punch
+fi
+cd $HOME/Termux-Backup
+bash Termux-Backup.sh ;;
+
+2)echo " "
+echo " "
+echo " "
+echo -e "$red                You are going to$red Restore termux$rset "
+echo " "
+echo -e "$grn                  Press$ylo ENTER$grn to continue$rset"
+echo " "
+read nitro
+cd $HOME/Termux-Backup/bars
+echo " "
+echo -e "$ylo         To terminate the process click$red 't' $ylo or to continue click on$grn ENTER$rset"
+read choice
+if [ $choice = 't' ] ; then
+echo -e "$red                   Are you sure? Press$grn ENTER$red to exit$rset"
+read nitroo
+cd $HOME/Termux-Backup
+bash Termux-Backup.sh
+else
+bash restore.sh
+echo " "
+echo " "
+echo -e "$grn ＞＞＞＞＞＞＞$ylo [EXIT]$grn ＜＜＜＜＜＜＜＜＜$rset"
+echo " "
+echo -e "$cyan             click$grn ENTER$cyan to go back into Termux-Backup menu$rset"
+echo " "
+read punch
+fi
+cd $HOME/Termux-Backup
+bash Termux-Backup.sh ;;
+3)echo " "
+echo " "
+echo " "
+echo -e "$red             You are going to$grn Update the$red Script$rset "
+echo " "
+echo -e "$grn                 Press$ylo ENTER$grn to continue$rset"
+echo " "
+read mon
+cd $HOME/Termux-Backup/bars
+echo " "
+echo -e "$ylo         To terminate the process click$red 't' $ylo or to continue click on$grn ENTER$rset"
+read choice
+if [ $choice = 't' ] ; then
+echo -e "$red                 Are you sure? Press$grn ENTER$red to exit$rset"
+read monn
+cd $HOME/Termux-Backup
+bash Termux-Backup.sh
+else
+bash update.sh
+echo " "
+echo " "
+echo -e "$grn>$ylo [EXIT]$grn<$rset"
+echo " "
+echo -e "$cyan        click$grn ENTER$cyan to go back into Termux-Backup menu$rset"
+echo " "
+read punch
+fi
+cd $HOME/Termux-Backup
+bash Termux-Backup.sh ;;
+4)
+sleep 2.0
+clear
+echo " "
+echo -e "$grn
+     ____  _  _  ____    ____  _  _  ___
+    (  _ \( \/ )( ___)  (  _ \( \/ )( ___)
+     ) _ < \  /  )__)    ) _ < \  /  )__)
+    (____/ (__) (____)  (____/ (__) (____)...$cyan Visit github.com/HackWeiser360 for more tools buddy.$rset"
+echo " "
+echo " "
+cd $HOME
+esac
